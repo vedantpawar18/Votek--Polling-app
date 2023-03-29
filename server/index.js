ee7b68906765ea485ed9e658ca64d157afb23342
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2023-03-29 19:41:20 +0530

    api for fetching user details

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 const express = require("express")
 const cors = require('cors');
 const {userController}= require("./routes/user.routes")
-const {firebaseController}= require("./routes/poll.firebase.routes")
+// const {firebaseController}= require("./routes/poll.firebase.routes")
 const {Connection, firebase} = require("./config/db");
 const authController=require("./routes/signin.routes")
 const {pollController}=require("./routes/poll.routes")
 const app = express();
 const PORT = process.env.PORT || 8080;
 const http = require('http');
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 const server = http.createServer(app);
 
-const fireDb = firebase.database(); 
-const ref = fireDb.ref("polls");
+// const fireDb = firebase.database(); 
+// const ref = fireDb.ref("polls");
 
 app.use(express.json());
 app.use(cors());
@@ -22,15 +22,15 @@ app.get("/", (req, res) => {
   });
 
 app.use("/user", userController);
-app.use("/firebase", firebaseController);
+// app.use("/firebase", firebaseController);
 app.use("/auth", authController);
 app.use("/poll",pollController);
 
 // ---------------Socket.io setup to get live changes ------->
-ref.on("value", (snapshot) => { 
-  io.emit("data", snapshot.val());
-});
-const io = new Server(server);
+// ref.on("value", (snapshot) => { 
+//   io.emit("data", snapshot.val());
+// });
+// const io = new Server(server);
 // ----------------------------------------------------------->
 
 server.listen(PORT, async () => {
